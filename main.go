56a4f788541b056d42d3e74832be30930c94ef73
Oleg Zdornyy <oleg@axiomzen.co>
2016-07-10 23:42:52 -0700

    able to successfully send binary/audio data to wit.ai

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
+
+type Response struct {
+	MsgID string `json:"msg_id"`
+	Text  string `json:"_text"`
+}
 
 func main() {
 	apiToken := os.Getenv("WIT_TOKEN")
@@ -14,10 +20,30 @@ func main() {
 	}
 	witAiSpeechURL := "https://api.wit.ai/speech"
 
-	request, err := http.NewRequest("POST", witAiSpeechURL, nil)
+	sampleFile, err := os.Open("sample.wav")
+	if err != nil {
+		log.Fatal("Error while opening sample file: ", err)
+	}
+
+	request, err := http.NewRequest("POST", witAiSpeechURL, sampleFile)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 	}
+
 	request.Header.Set("Authorization", "Bearer "+apiToken)
 	request.Header.Set("Content-Type", "audio/wav")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		fmt.Printf("Error while sending request!")
+		log.Fatal(error)
+	}
+
+	var sample = Response{}
+
+	json.NewDecoder(response.Body).Decode(&sample)
+
+	fmt.Printf("Result ID: %s\n", sample.MsgID)
+	fmt.Printf("Result _text: %s\n", sample.Text)
 }
